57e4c8dda4a10139d22d92a59dd0218d592c7523
Eric Araya <earaya.mza@gmail.com>
2022-09-04 18:17:44 -0300

    subida proyecto final

---
+++ js/carrito.js
@@ -0,0 +1,138 @@
+function itemsEnCarrito() {
+  const seleccion = itemsElegidosCarritoLS();
+
+  let contenido = "";
+
+  if (seleccion.length == 0) {
+    contenido = `<div class="alert alert-secondary text-center col-4 mx-auto" role="alert"> <b>Tu Carrito esta Vacio !!!</b></div>`;
+  } else {
+    contenido += `
+    <table class="table table-refresh table-hover table-borderless bg-white">
+      <thead>
+      <tr class="bg-dark">
+        <th class="text-white text-center">PRODUCTO</th>
+        <th class="text-white text-center"></th>
+        <th class="text-white text-center">CANTIDAD </th>
+        <th class="text-white text-center"></th>
+        <th class="text-white text-center">PRECIO</th>
+        <th class="text-white text-center"></th>
+      </tr>
+    </thead>`;
+    seleccion.forEach((vino) => {
+      contenido += `
+        <tbody >
+          <tr>
+            <td class="align-middle fw-bold ">${vino.nombre}</td>
+            <td id="${vino.id}" class="text-center container-eliminar" width="30"><a href="" class="btn">
+            <img src="/image/quitar.png" alt="cart" class="btn-eliminar" title="Quitar" width="22"><span class="badge bg-secondary"></span></img>
+            
+          </a></td>
+          <td class=" container-tabla text-center align-middle fw-bold" width="30">${vino.cantidad}</td>
+          <td id="${vino.id}" class="text-center container-agregar" width="30"><a href="" class="btn">
+            <img src="/image/agregar.png" alt="cart" class="btn-sumar"  title="Agregar"width="22"><span class="badge bg-secondary"></span></img>
+          </a></td>
+            <td class="text-center fw-bold" width="130">$ ${(vino.precio * vino.cantidad).toFixed(2)}</td>
+            <td class="text-center" width="80"><a href="" class="btn">
+            <img id="${vino.id}" src="/image/tacho.png" alt="cart" class="btn-eliminar container-eliminar" title="Eliminar Articulo" width="18"><span class="badge bg-secondary"></span></img>
+          </a></td>
+          </tr>
+        </tbody>`;
+    });
+
+    contenido += `<tfoot class="bg-dark">
+      <th class="text-white bg-dark">TOTAL A PAGAR</th>
+      <th class="text-white bg-dark"></th>
+      <th class="text-white bg-dark text-center"></th>
+      <th class="text-white bg-dark text-center"></th>
+      <th class="text-white bg-dark text-center">$ ${descuento().toFixed(2)}</th>
+      <th class="text-white bg-dark text-center"></th>
+    </tfoot>
+    <tfoot class="bg-dark">
+      <th class="text-white bg-dark">TOTAL</th>
+      <th class="text-white bg-dark"></th>
+      <th class="text-white bg-dark text-center"></th>
+      <th class="text-white bg-dark text-center"></th>
+      <th class="text-white bg-dark text-center">$ ${totalCompra().toFixed(2)} </th>
+      <th class="text-white bg-dark text-center"></th>
+    </tfoot>
+    <tfoot class="bg-dark">
+      <th class="text-white bg-dark">DESCUENTOS OBTENIDOS</th>
+      <th class="text-white bg-dark"></th>
+      <th class="text-white bg-dark text-center"></th>
+      <th class="text-white bg-dark text-center"></th>
+      <th class="text-white bg-dark text-center">$ ${(totalCompra () - descuento()).toFixed(2)} </th>
+      <th class="text-white bg-dark text-center"></th>
+    </tfoot></table>`;
+    contenido += `<button type="button" class="btn btn-success col-4 mx-auto" width="80">FINALIZAR COMPRA</button>`;
+  }
+  document.getElementById("productos").innerHTML = contenido;
+}
+
+
+function eliminarItem() {
+  const itemsCarrito = itemsElegidosCarritoLS();
+
+  const botones = document.querySelectorAll(".btn-eliminar");
+
+  botones.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+      let itemId = parseInt(e.target.closest(".container-eliminar").id);
+
+      let pos = itemsCarrito.findIndex((vino) => vino.id === itemId);
+
+      itemsCarrito[pos].cantidad -= 1;
+      
+      if(itemsCarrito[pos].cantidad == 0){
+        itemsCarrito.splice(pos, 1);
+      }
+      guardarStockCarritoLS(itemsCarrito);
+      itemsEnCarrito();
+      botonCarrito();
+      
+    });
+  });
+ 
+}
+
+function sumarItem() {
+  const itemsCarrito = itemsElegidosCarritoLS();
+  const botones = document.querySelectorAll(".btn-sumar");
+
+  botones.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+      let itemId = parseInt(e.target.closest(".container-agregar").id);
+      let pos = itemsCarrito.findIndex((vino) => vino.id === itemId);
+
+      itemsCarrito[pos].cantidad += 1;
+      
+      if(itemsCarrito[pos].cantidad == 0){
+        itemsCarrito.push(pos, 1);
+      }
+      guardarStockCarritoLS(itemsCarrito);
+      itemsEnCarrito();
+      botonCarrito();
+    });
+ 
+  });
+}
+
+function vaciarCarrito(){
+
+ localStorage.removeItem("elegidos");
+
+ let contenido = `<button type="button" class="btn">
+ <span class="badge bg-secondary"></span>
+</button>`;
+
+document.getElementById("vaciar_cart").innerHTML = contenido;
+
+itemsEnCarrito();
+botonCarrito();
+
+}
+
+itemsEnCarrito();
+agregarAlCarrito();
+eliminarItem();
+sumarItem();
+botonCarrito();
---
+++ js/main.js
@@ -0,0 +1,132 @@
+
+const array = [];
+
+function jsonALocalStorage() {
+
+
+  fetch("/json/articulos.json")
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+      if (typeof data === "string") {
+        data = JSON.parse(data);
+      } else
+        data.forEach(function (item) {
+          array.push(item);
+        });
+
+      localStorage.setItem("json", JSON.stringify(array));
+    });
+}
+const carrito = [];
+
+function stockEnLS() {
+  return JSON.parse(localStorage.getItem("json")) || [];
+}
+
+function guardarStockCarritoLS(array) {
+  localStorage.setItem("elegidos", JSON.stringify(array));
+}
+
+function itemsElegidosCarritoLS() {
+  return JSON.parse(localStorage.getItem("elegidos")) || [];
+}
+
+function stock() {
+  const stockTienda = stockEnLS();
+
+  let contenido = "";
+
+  stockTienda.forEach((vino) => {
+    contenido += `<div id="${vino.id}" class= "col-md-4 container-tarjeta" style="width: 300px;">
+      <div class="card text-center text-white fw-bold mb-3" id="card-vinos" >
+      <img src="image/${vino.imagen}" class="card-img-top" alt="${vino.nombre}">
+      <div class="card-body">
+        <h6 class="card-title fw-bold ">${vino.varietal}</h6>
+        <p class="card-text">${vino.nombre}</p>
+        <p class="card-text">$ ${vino.precio}</p>
+        <button type="button" class="btn btn-success btn-agregar">Agregar al Carrito</button>
+        
+      </div>
+    </div>
+    </div>`;
+  });
+  document.getElementById("productos").innerHTML = contenido;
+}
+
+function agregarAlCarrito() {
+  const itemsCarrito = itemsElegidosCarritoLS();
+  const botones = document.querySelectorAll(".btn-agregar");
+
+  botones.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+      let itemId = parseInt(e.target.closest(".container-tarjeta").id);
+      let pos = itemsCarrito.findIndex((vino) => vino.id === itemId);
+      if (pos > -1) {
+        itemsCarrito[pos].cantidad += 1;
+      } else {
+        let item = array.find((vino) => vino.id === itemId);
+        item.cantidad = 1;
+        itemsCarrito.push(item);
+      }
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: "Producto Agregado!!",
+        showConfirmButton: false,
+        timer: 1000,
+      });
+      guardarStockCarritoLS(itemsCarrito);
+      botonCarrito();
+    });
+  });
+}
+
+function botonCarrito() {
+  let contenido = `<button type="button" class="btn">
+    <img src="/image/cart.png" alt="cart" width="42"></img>
+    <span class="badge bg-secondary">${ArticulosEnCarrito()}</span>
+  </button>`;
+
+  document.getElementById("btn-cart").innerHTML = contenido;
+}
+
+function ArticulosEnCarrito() {
+  const itemsCarrito = itemsElegidosCarritoLS();
+
+  return itemsCarrito.reduce((sumatoria, vino) => sumatoria + vino.cantidad, 0);
+}
+
+function descuento() {
+  const itemsCarrito = itemsElegidosCarritoLS();
+
+  let dto = itemsCarrito.reduce(
+    (sumatoria, vino) => sumatoria + vino.cantidad,
+    0
+  );
+
+  if (dto >= 4) {
+    return itemsCarrito.reduce(
+      (sumatoria, vino) => sumatoria + vino.cantidad * vino.precio * 0.85,
+      0
+    );
+  }
+
+  return itemsCarrito.reduce(
+    (sumatoria, vino) => sumatoria + vino.cantidad * vino.precio,
+    0
+  );
+}
+
+function totalCompra() {
+  const itemsCarrito = itemsElegidosCarritoLS();
+
+  return itemsCarrito.reduce(
+    (sumatoria, vino) => sumatoria + vino.cantidad * vino.precio,
+    0
+  );
+}
+
+jsonALocalStorage();
+stock();
+agregarAlCarrito();
+botonCarrito();
